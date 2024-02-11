b90b091d1b5a1da963807c6f25eb05bf7421392d
Sandun Induranga <sandutharu40@gmail.com>
2024-02-11 20:37:07 +0530

    Content Updated

--- App.jsx
+++ App.jsx
@@ -1,13 +1,18 @@
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
-
+import * as Speech from "expo-speech";
 import React from "react";
 
 const App = () => {
+  const _onPressSpeech = () => {
+    Speech.speak("Hello, world!");
+  };
+
   return (
     <View>
       <Text>Open up App.jsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text onPress={_onPressSpeech}>Press me to hear speech</Text>
     </View>
   );
 };
