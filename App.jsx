28b7948f281098172f4b78cbc04fcc4b5c859580
Sandun Induranga <sandutharu40@gmail.com>
2024-02-11 20:15:00 +0530

    first commit

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+
+import React from "react";
+
+const App = () => {
+  return (
+    <View>
+      <Text>Open up App.jsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+
+export default App;
