60f1836672044a1411323b80b60ee474dd7edfce
Sandun Induranga <sandutharu40@gmail.com>
2024-02-11 22:04:33 +0530

    Content Updated

--- components/Note/Note.jsx
+++ components/Note/Note.jsx
@@ -3,7 +3,14 @@ import { View, Text } from "react-native";
 
 const Note = ({ note }) => {
   return (
-    <View style={{ backgroundColor: "powderblue" }}>
+    <View
+      style={{
+        backgroundColor: "powderblue",
+        padding: 4,
+        margin: 20,
+        borderRadius: 4,
+      }}
+    >
       <Text>{note.title}</Text>
     </View>
   );
