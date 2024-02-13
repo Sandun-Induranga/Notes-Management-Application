3cdea33e7f5fd54199c45d748070fe7aa8c0bf17
Sandun Induranga <sandutharu40@gmail.com>
2024-02-13 16:47:03 +0530

    Updated

--- components/Note/Note.jsx
+++ components/Note/Note.jsx
@@ -12,34 +12,33 @@ const Note = ({ note }) => {
         padding: 4,
         margin: 20,
         borderRadius: 4,
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 10,
+        padding: 10,
       }}
     >
       <Text
         style={{
           fontSize: 20,
           fontWeight: "bold",
-          color: "white",
+          color: "gray",
         }}
       >
         {note.title}
       </Text>
+      <Image
+        source={{ uri: `data:image/png;base64,${note.image}` }}
+        width={150}
+        height={150}
+      />
       <Text
         style={{
           fontSize: 16,
           color: "white",
         }}
-      >
-        {note.content}
-      </Text>
-      <Text
-        style={{
-          fontSize: 16,
-          color: "white",
-        }}
-      >
-        {note.image}
-      </Text>
-      <Image source={{ uri: note.image }} />
+      ></Text>
     </View>
   );
 };
